f2defa2741de11dcbbeeea056374354f47e159d7
Yana Khoruzha <i.am.kitiket@gmail.com>
2023-05-12 20:50:58 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.wrapper}>
+        <Navigation/>
+      </div>
     </div>
   );
 }
